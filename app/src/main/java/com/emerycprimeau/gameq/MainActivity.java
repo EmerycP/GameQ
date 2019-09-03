5b9e50c73017d06c19378bee471cb38df7a8c243
Primeau Émeryc <1761934@cegepmontpetit.ca>
2019-09-03 14:42:10 -0400

    Added Drawer / Doesnt work tho

--- app/src/main/java/com/emerycprimeau/gameq/MainActivity.java
+++ app/src/main/java/com/emerycprimeau/gameq/MainActivity.java
@@ -2,7 +2,14 @@ package com.emerycprimeau.gameq;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.ActionProvider;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }

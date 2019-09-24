804453eb0379cf95e82fded3e3bc26efe0f811ee
Primeau Émeryc <1761934@cegepmontpetit.ca>
2019-09-24 15:09:21 -0400

    Added base Service/Retrofit

--- app/src/main/java/com/emerycprimeau/gameq/http/RetrofitUtil.java
+++ app/src/main/java/com/emerycprimeau/gameq/http/RetrofitUtil.java
@@ -1,4 +1,18 @@
 package com.emerycprimeau.gameq.http;
 
+import retrofit2.Retrofit;
+
 public class RetrofitUtil {
+
+      //Exemple de retrofit
+//  public static Service get(){
+//    Retrofit retrofit = new Retrofit.Builder()
+//            .addConverterFactory(ScalarsConverterFactory.create())
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .baseUrl("https://api.github.com/")
+//            .build();
+//
+//    Service service = retrofit.create(Service.class);
+//  }
+
 }
--- app/src/main/java/com/emerycprimeau/gameq/http/Service.java
+++ app/src/main/java/com/emerycprimeau/gameq/http/Service.java
@@ -1,4 +1,12 @@
 package com.emerycprimeau.gameq.http;
 
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface Service {
+
+      //Exemple de requête
+//    @GET("users/{user}/repos")
+//    Call<List<Repo>> listRepos(@Path("user") String user);
+
 }

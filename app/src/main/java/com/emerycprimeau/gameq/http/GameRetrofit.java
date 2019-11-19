a2f06f9ef9cbcff357517673006f5aac200a99cb
Primeau Émeryc <1761934@cegepmontpetit.ca>
2019-11-19 14:47:06 -0500

    Https & Fixes

--- app/src/main/java/com/emerycprimeau/gameq/http/GameRetrofit.java
+++ app/src/main/java/com/emerycprimeau/gameq/http/GameRetrofit.java
@@ -3,6 +3,13 @@ package com.emerycprimeau.gameq.http;
 import com.emerycprimeau.gameq.http.mock.Mock;
 import com.emerycprimeau.gameq.http.mock.ServiceMock;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -18,7 +25,7 @@ public class GameRetrofit {
     Retrofit retrofit = new Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .client(getClient())
-            .baseUrl("http://10.0.2.2:8080/api/")
+            .baseUrl("https://10.0.2.2:8443/api/")
             .build();
 
     Service service = retrofit.create(Service.class);
@@ -28,17 +35,47 @@ public class GameRetrofit {
 
     public static OkHttpClient getClient(){
         try {
-            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+                OkHttpClient.Builder builder = new OkHttpClient.Builder();
+                final TrustManager[] trustAllCerts = new TrustManager[]{
+                        new X509TrustManager() {
+                            @Override
+                            public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                            }
 
-            // Adds logging capability to see http exchanges on Android Monitor
-            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-            builder = builder.addInterceptor(interceptor);
-            return builder.build();
-        }catch(Exception e){
-            e.printStackTrace();
-            return null;
-        }
+                            @Override
+                            public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                            }
+
+                            @Override
+                            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                                return new java.security.cert.X509Certificate[]{};
+                            }
+                        }
+                };
+                // Install the all-trusting trust manager
+                final SSLContext sslContext = SSLContext.getInstance("SSL");
+                sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+                // Create an ssl socket factory with our all-trusting manager
+                final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+                // configure the builder to accept all SSL certificates
+                builder = builder.sslSocketFactory(sslSocketFactory);
+                // configure the builder to accept all hostnames includint localhost
+                builder = builder.hostnameVerifier(new HostnameVerifier() {
+                    @Override
+                    public boolean verify(String hostname, SSLSession session) {
+                        return true;
+                    }
+                });
+                // Adds logging capability to see http exchanges on Android Monitor
+                HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+                interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+                builder = builder.addInterceptor(interceptor);
+                return builder.build();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
     }
 
 
